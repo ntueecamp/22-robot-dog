b57314a2d3a9d23cb64280c986a354bc850c556c
tom1484 <angelus0918866416@gmail.com>
2022-06-23 17:39:16 +0800

    EDITOR-#1. Upload ESP32Servo library

---
+++ ESP32Servo.h
@@ -0,0 +1,42 @@
+#ifndef ESP32Servo_h
+#define ESP32Servo_h
+
+#include "Arduino.h"
+
+#define MID_DUTY         1479
+#define HALF_DUTY_WIDTH  500
+#define MIN_DUTY         (MID_DUTY - HALF_DUTY_WIDTH)
+#define MAX_DUTY         (MID_DUTY + HALF_DUTY_WIDTH)
+
+#define PWM_FREQ         50
+#define PWM_CYCLE        (1000000.0 / PWM_FREQ)
+#define TIMER_WIDTH      16
+#define TIMER_RESOLUTION ((1 << TIMER_WIDTH) - 1)
+
+
+class ESP32Servo {
+
+private:
+
+  int channel;
+  int servoPin;
+
+  double minSpeed;
+  double maxSpeed;
+
+  // re-define map function with double type
+  double fmap(double val, double min1, double max1, double min2, double max2);
+
+public:
+
+  // specify channel and pin of led_c, and the range of servo speed
+  void initialize(int _channel, int _servoPin, double _minSpeed, double _maxSpeed);
+
+  // set servo speed
+  void write(double R);
+  // calculate speed by mapping before setting speed
+  double mapWrite(double val, double minVal, double maxVal);
+
+};
+
+#endif
