feb2de0e9f038d367f0b12eef7e3f96df728f6c5
tom1484 <angelus0918866416@gmail.com>
2022-06-28 18:47:45 +0800

    feature-#14. Update methods and add more comments

--- Leg.cpp
+++ Leg.cpp
@@ -7,39 +7,13 @@ void Leg::initialize() {
   Leg::leftChannel = DEFAULT_LEFT_CHANNEL;
   Leg::rightChannel = DEFAULT_RIGHT_CHANNEL;
 
-  Leg::initializeServo();
-}
-
-void Leg::initialize(int _leftPin, int _rightPin) {
-  Leg::leftPin = _leftPin;
-  Leg::rightPin = _rightPin;
-  Leg::leftChannel = DEFAULT_LEFT_CHANNEL;
-  Leg::rightChannel = DEFAULT_RIGHT_CHANNEL;
-
-  Leg::initializeServo();
-}
-
-void Leg::initialize(int _leftChannel, int _rightChannel, int _leftPin, int _rightPin) {
-  Leg::leftPin = _leftPin;
-  Leg::rightPin = _rightPin;
-  Leg::leftChannel = _leftChannel;
-  Leg::rightChannel = _rightChannel;
-
-  Leg::initializeServo();
-}
-
-void Leg::initializeServo() {
   Serial.printf("%d, %d, %f, %f\n", leftChannel, leftPin, MIN_SPEED, MAX_SPEED);
   Leg::leftServo.initialize(leftChannel, leftPin, MIN_SPEED, MAX_SPEED);
   Leg::rightServo.initialize(rightChannel, rightPin, MIN_SPEED, MAX_SPEED);
 }
 
 void Leg::write(double leftR, double rightR) {
-  Leg::leftServo.write(leftR);
-  Leg::rightServo.write(rightR);
-}
-
-double Leg::mapWrite(double leftVal, double rightVal, double minVal, double maxVal) {
-  Leg::leftServo.mapWrite(leftVal, minVal, maxVal);
-  Leg::rightServo.mapWrite(rightVal, minVal, maxVal);
+  // servos are placed in different rotation, so one of them should be reversed
+  Leg::leftServo.write(leftR * (1.0));
+  Leg::rightServo.write(rightR * (-1.0));
 }
