15510524c5fd172f2cb929d71d9eeceff5536d01
FrSh28 <37129124+FrSh28@users.noreply.github.com>
2022-07-19 21:55:15 +0800

    Change test for interact_inputs (#75)

--- test/cap_test/cap_test.ino
+++ test/cap_test/cap_test.ino
@@ -2,19 +2,9 @@
 
 #define CAP_TOUCH_PIN 4
 
-int count = 0;
-
-void IRAM_ATTR onCapISR()
-{
-  count++;
-}
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-
-  // Method 2: interrupt (pt. 1)
-  // touchAttachInterrupt(CAP_TOUCH_PIN, onCapISR, 50);
 }
 
 void loop() {
@@ -22,9 +12,4 @@ void loop() {
   // Method 1: periodically read value
   Serial.println(touchRead(CAP_TOUCH_PIN));
   delay(100);
-
-  // Method 2: interrupt (pt. 2)
-  // Serial.println(count);
-  // count = 0;
-  // delay(1000);
 }
