a69c083a3cf0b2117d12f1536b7842280a48c989
FrSh28 <37129124+FrSh28@users.noreply.github.com>
2022-07-19 21:54:18 +0800

    Change test for interact_inputs

--- test/switch_test/switch_test.ino
+++ test/switch_test/switch_test.ino
@@ -2,22 +2,11 @@
 
 #define LIMIT_SWITCH_PIN 12
 
-int count = 0;
-
-void IRAM_ATTR onLimitSwitchISR()
-{
-  count++;
-}
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
 
-  pinMode(LIMIT_SWITCH_PIN, INPUT);
-  // pinMode(LIMIT_SWITCH_PIN, INPUT_PULLDOWN);
-
-  // Method 2: interrupt (pt. 1)
-  // attachInterrupt(LIMIT_SWITCH_PIN, onLimitSwitchISR, RISING);
+  pinMode(LIMIT_SWITCH_PIN, INPUT_PULLUP);
 }
 
 void loop() {
@@ -25,9 +14,4 @@ void loop() {
   // Method 1: periodically read value
   Serial.println(digitalRead(LIMIT_SWITCH_PIN));
   delay(100);
-
-  // Method 2: interrupt (pt. 2)
-  // Serial.println(count);
-  // count = 0;
-  // delay(1000);
 }
